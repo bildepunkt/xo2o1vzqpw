b8226b14952f875599d10f1177cd65c34819a516
Chris Peters <cpeters@pingidentity.com>
2019-02-18 20:47:44 -0800

    privatize Sprite members so we can do stuff with setters

--- src/Sprite.ts
+++ src/Sprite.ts
@@ -1,29 +1,105 @@
 export type SpriteMap = Array<number[]>;
+export type SpriteRotation = 0 | 90 | 180 | 270;
 
 export interface SpriteOpts {
-  map?:SpriteMap;
   flipX?:boolean;
   flipY?:boolean;
+  map?:SpriteMap;
+  rotation?:SpriteRotation;
+}
+
+export interface SpritePrivates {
+  flipX:boolean;
+  flipY:boolean;
+  map:SpriteMap;
+  mapOriginal:SpriteMap;
+  rotation:SpriteRotation;
 }
 
 export class Sprite {
-  private static defaults:SpriteOpts = {
-    map: [],
+  private static defaults:SpritePrivates = {
     flipX: false,
-    flipY: false
+    flipY: false,
+    map: [],
+    mapOriginal: [],
+    rotation: 0
   };
-  public map:SpriteMap;
-  public flipX:boolean;
-  public flipY:boolean;
+  private privates:SpritePrivates;
 
-  constructor (opts:SpriteOpts) {
-    opts = Object.assign({}, Sprite.defaults, opts);
-    this.map = opts.map;
-    this.flipX = opts.flipX;
-    this.flipY = opts.flipY;
+  constructor (opts:SpriteOpts = {}) {
+    this.privates = Object.assign({}, Sprite.defaults, opts);
+    this.privates.mapOriginal = this.privates.map;
   }
 
   public cloneMap ():SpriteMap {
-    return this.map.map((row:number[]) => row.map(index => index));
+    return this.privates.map.map((row:number[]) => row.map(index => index));
+  }
+
+  public cloneMapOriginal ():SpriteMap {
+    return this.privates.mapOriginal.map((row:number[]) => row.map(index => index));
+  }
+
+  public get map ():SpriteMap {
+    return this.privates.map;
+  }
+
+  public get flipX ():boolean {
+    return this.privates.flipX;
+  }
+
+  public get flipY ():boolean {
+    return this.privates.flipY;
+  }
+
+  public get rotation ():SpriteRotation {
+    return this.privates.rotation;
+  }
+
+  public set map (value:SpriteMap) {
+    this.privates.map = value;
+    this.privates.mapOriginal = value;
+  }
+
+  public set flipX (value:boolean) {
+    if (value !== this.privates.flipX) {
+      for (let row of this.privates.map) {
+        row = row.reverse();
+      }
+      this.privates.flipX = value;
+    }
+  }
+
+  public set flipY (value:boolean) {
+    if (value !== this.privates.flipY) {
+      this.privates.map = this.privates.map.reverse();
+      this.privates.flipY = value;
+    }
+  }
+
+  public set rotation (value:SpriteRotation) {
+    if (value !== this.privates.rotation) {
+      const mapOriginal = this.cloneMapOriginal();
+      const rotatedMap = [];
+
+      switch (value) {
+        case 0:
+          // do nothing
+          break;
+        case 90:
+          // for (let y = 0; y < mapOriginal.length; y++) {
+          //   for (let x = 0; x < mapOriginal[y].length; x++) {
+          //     //
+          //   }
+          // }
+          break;
+        case 180:
+          //
+          break;
+        case 270:
+          //
+          break;
+      }
+      this.privates.rotation = value;
+    }
   }
 }
