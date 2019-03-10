231e5388acf4aba49cd556ab41e0a72d8af6ffcc
Chris Peters <cpeters@pingidentity.com>
2019-03-09 19:58:44 -0800

    add clear method and fix bixel render in RenderEngine

--- src/RenderEngine.ts
+++ src/RenderEngine.ts
@@ -9,17 +9,22 @@ export default class RenderEngine implements IRenderEngine {
   private config:Config;
   private dom:DOM;
 
-  constructor(config:Config, dom:DOM) {
+  constructor (config:Config, dom:DOM) {
     this.config = config;
     this.dom = dom;
   }
 
-  public renderBixel = (x:number, y:number, colourValue:string):void => {
-    const bpp = this.config.bpp;
+  public clear ():void {
+    const { bpp, resolution } = this.config;
+    this.dom.context.clearRect(0, 0, resolution.width * bpp, resolution.height * bpp);
+  }
+
+  public renderBixel (x:number, y:number, colourValue:string):void {
+    const { bpp } = this.config;
     this.dom.context.fillStyle = colourValue;
     this.dom.context.fillRect(
-      Math.floor(x * bpp),
-      Math.floor(y * bpp),
+      Math.floor(x) * bpp,
+      Math.floor(y) * bpp,
       bpp,
       bpp
     );
